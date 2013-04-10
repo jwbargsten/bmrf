c240743daedd6ce1b2d8349d2ea0cccb8280070e
Joachim Bargsten <jw@bargsten.org>
2013-04-10 18:31:12 +0200

    .

--- tests/test_basic.R
+++ tests/test_basic.R
@@ -5,5 +5,5 @@ FILEann="test_data/go_labels.up.tsv"
 FILEclust="test_data/ipr_labels.tsv"
 outprobsfile="/tmp/yntest.out"
 
-bmrf <- read.bmrf(FILEnet, FILEann, FILEclust, minGOsize=6,minFDsize=6, maxGOsize=Inf, maxFDsize=Inf )
+bmrf <- read.bmrf(FILEnet, FILEann, FILEclust, minGOsize=3,minFDsize=3, maxGOsize=Inf, maxFDsize=Inf )
 set.seed(10); p <- predict.bmrf(bmrf, file=outprobsfile, format="3col", verbose=TRUE)
